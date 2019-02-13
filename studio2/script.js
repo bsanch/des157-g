9cc2d1c333220ce003a85a6b9a14457bc2921616
Bria Sanchez <briasnchz@gmail.com>
2019-02-12 16:47:31 -0800

    studio2 and portal index

--- studio2/script.js
+++ studio2/script.js
@@ -1,21 +1,31 @@
 'use strict';
 console.log('reading js');
 
-document.querySelector("#backpack").addEventListener("click", showStuff);
-document.querySelector("#hide").addEventListener("click", hideStuff);
+// function backpack() {
+//   var button = document.querySelector("#contents");
+//   if (button.style.display === "block") {
+//     button.style.display = "none";
+//   } else {
+//     button.style.display = "block";
+//   }
+// }
 
-var sketchbook = document.querySelector("#sketchbook");
-var usb = document.querySelector("#usb");
-var pillbox = document.querySelector("#pillbox");
 
-function showStuff(evt){
-  console.log('showStuff');
-
-  //touch.className = "show";
-  document.getElementById("touch").className='show';
-}
-
-function hideStuff() {
-  document.getElementById("touch").style.display = "none";
-  //touch.className = "hide";
-}
+// document.querySelector("#backpack").addEventListener("click", showStuff);
+// document.querySelector("#hide").addEventListener("click", hideStuff);
+//
+// var sketchbook = document.querySelector("#sketchbook");
+// var usb = document.querySelector("#usb");
+// var pillbox = document.querySelector("#pillbox");
+//
+// function showStuff(evt){
+//   console.log('showStuff');
+//
+//   //touch.className = "show";
+//   document.getElementById("touch").className='show';
+// }
+//
+// function hideStuff() {
+//   document.getElementById("touch").style.display = "none";
+//   //touch.className = "hide";
+// }
